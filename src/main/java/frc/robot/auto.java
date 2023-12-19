0a11eb2507f6c3ba4d6d0ff3a6034548f81f35cc
SallyLin0907 <ples104013@gmail.com>
2023-12-19 20:29:30 +0800

    [change] auto

--- src/main/java/frc/robot/auto.java
+++ src/main/java/frc/robot/auto.java
@@ -1,8 +1,10 @@
 package frc.robot;
 
 import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+import com.kauailabs.navx.frc.AHRS;
 
-import edu.wpi.first.wpilibj.ADXRS450_Gyro;
+// import edu.wpi.first.wpilibj.ADXRS450_Gyro;
+import edu.wpi.first.wpilibj.SPI;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.motorcontrol.VictorSP;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
@@ -11,103 +13,119 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 public class auto {
     public static SendableChooser<String> chooser = new SendableChooser<String>();
     public static final String kshootball = "shootball";
-    public static final String kdonothing = "donothing";
-    public static final String kmove = "move";
+    public static final String outcommunity = "outcommunity";
+    public static final String donothing = "donothing";
     public static String autoselected;
     public static Timer timer = new Timer();
-    public static WPI_VictorSPX L = new WPI_VictorSPX(6);
-    public static WPI_VictorSPX R = new WPI_VictorSPX(7);
-    public static WPI_VictorSPX top = new WPI_VictorSPX(0);
-    // public static VictorSP L = new VictorSP(6);
-    // public static VictorSP R = new VictorSP(7);
+    // public static WPI_VictorSPX L = new WPI_VictorSPX(6);
+    // public static WPI_VictorSPX R = new WPI_VictorSPX(7);
+    // public static WPI_VictorSPX top = new WPI_VictorSPX(0);
+    public static VictorSP L = new VictorSP(6);
+    public static VictorSP R = new VictorSP(7);
 
-    public static ADXRS450_Gyro gyro = new ADXRS450_Gyro();
+    public static AHRS gyro = new AHRS(SPI.Port.kMXP);
+
     public static int a = 0;
 
     public static void setting() {
         chooser.setDefaultOption("shootball", kshootball);
-        chooser.addOption("donothing", kdonothing);
-        chooser.addOption("move", kmove);
+        chooser.addOption("donothing", donothing);
+        chooser.addOption("outcommunity", outcommunity);
     }
 
     public static void start() {
         SmartDashboard.putData("Auto Choise", chooser);
-        autoselected = chooser.getSelected();
+       // autoselected = chooser.getSelected();
+        gyro.reset();
+        timer.reset();
+        timer.start();
     }
 
     public static void loop() {
-        switch (autoselected) {
+        switch (chooser.getSelected()) {
             case kshootball:
-                timer.reset();
                 kshootball();
-                break;
-            case kdonothing :
-                 L.set(0);
-                 R.set(0);
-                 break;
-            case kmove:
-                timer.reset();
-                kmove();
-                break;
+            case donothing:
+                L.set(0);
+                R.set(0);
+            case outcommunity:
+                    if (timer.get() < 3) {
+                        L.set(-0.5);
+                        R.set(0.5);
+                    } else {
+                        timer.stop();
+                    }
 
+                
         }
-
     }
 
     public static void kshootball() {
-        gyro.reset();
-        if (a == 0) {
-            if (gyro.getAngle() > -45) {
-                R.set(0.3);
 
+        // if (a == 0) {
+        // if (gyro.getAngle() > -45) {
+        // R.set(0.3);
+
+        // } else {
+        // a = 1;
+        // timer.reset();
+        // timer.start();
+        // }
+        // } else if (a == 1) {
+
+        // if (timer.get() < 2) {
+        // L.set(-0.5);
+        // R.set(0.5);
+        // } else {
+        // a = 2;
+        // gyro.reset();
+        // }
+        // } else if (a == 2) {
+        // if (gyro.getAngle() < 90) {
+        // L.set(-0.3);
+
+        // } else {
+        // a = 3;
+        // }
+        // // }else if(a==3){
+        // // top.set(0.2);
+        // }
+        if (a == 0) {
+
+            if (timer.get() < 15) {
+                L.set(-0.2);
+                R.set(0.2   );
             } else {
+                timer.stop();
                 a = 1;
             }
-        } else if (a == 1) {
-            timer.start();
-            if (timer.get() < 2) {
-                L.set(-0.5);
-                R.set(0.5);
-            } else {
-                a = 2;
-                gyro.reset();
-            }
-        } else if (a == 2) {
-            if (gyro.getAngle() < 90) {
-                L.set(-0.3);
 
+        } else if (a == 1) {
+         if(gyro.getAngle()>90){
+            L.set(-0.3);
+            R.set(-0.3);
+         } else{
+            a=2;
+            timer.reset();
+            timer.start();
+         }
+
+        }else if(a==2){
+             if (timer.get() < 3) {
+                L.set(-0.2);
+                R.set(0.2);
             } else {
-                a = 3;
                 timer.reset();
                 timer.start();
+                a = 3;
             }
-            }else if(a==3){
-                if(timer.get()<=5){
-                    top.set(0.2);
-                }else{
-                    a = 4;
-                }
-            }
-    
+        }else if (a==3){
+            if(timer.get()<5){
+           // top.set(0.2);
+        }else{
+            a=4;
+        }
 
-    }
-    public static void kmove(){
-        gyro.reset();
-        if (a == 0) {
-            if (gyro.getAngle() > -45) {
-                R.set(0.3);
-
-            } else {
-                a = 1;
-            }
-        } else if (a == 1) {
-            timer.start();
-            if (timer.get() < 2) {
-                L.set(-0.5);
-                R.set(0.5);
-            } else {
-                a = 2;
-            }
     }
 }
 }
--- src/main/java/frc/robot/teleop.java
+++ src/main/java/frc/robot/teleop.java
@@ -3,21 +3,25 @@ package frc.robot;
 import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.motorcontrol.VictorSP;
 
 public class teleop {
-    public static WPI_VictorSPX L = new WPI_VictorSPX(6);
-    public static WPI_VictorSPX R = new WPI_VictorSPX(7);
-    public static WPI_VictorSPX top = new WPI_VictorSPX(0);
+    // public static WPI_VictorSPX L = new WPI_VictorSPX(6);
+    // public static WPI_VictorSPX R = new WPI_VictorSPX(7);
+    // public static WPI_VictorSPX top = new WPI_VictorSPX(0);
+    public static VictorSP L = new VictorSP(6);
+    public static VictorSP R = new VictorSP(7);
+
     public static Joystick joy = new Joystick(0);
     public static void loop(){
     L.set(-joy.getRawAxis(1));
     R.set(joy.getRawAxis(5));
-    if (joy.getRawButtonPressed(0)) {
-      top.set(0.2);
-    } else if (joy.getRawButtonPressed(1)) {
-      top.set(0.5);
-    } else if (joy.getRawButtonPressed(2)) {
-      top.set(0.7);
-    }
+    // if (joy.getRawButtonPressed(0)) {
+    //   top.set(0.2);
+    // } else if (joy.getRawButtonPressed(1)) {
+    //   top.set(0.5);
+    // } else if (joy.getRawButtonPressed(2)) {
+    //   top.set(0.7);
+    // }
     }
 }
